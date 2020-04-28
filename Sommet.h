ae2216e7259b173c1183c1f59262ae49a46e1adf
inesked <61450751+inesked@users.noreply.github.com>
2020-04-28 12:28:43 +0200

    sommet.h vecteur propre

--- Sommet.h
+++ Sommet.h
@@ -11,15 +11,32 @@ class Sommet
         int m_num;
         std::string m_nom;
         std::pair <double,double> m_coordonnees;
+        std::vector<const Sommet*> m_successeurs;
 
     public:
 
         Sommet(int num, std::string nom, std::pair<double,double> coordonnees);
         int getNum();
+
+        ///accesseur : pour la liste des successeurs
+        const std::vector<const Sommet*>& getSuccesseurs()const {return m_successeurs;}
+        /* pour ajouter un successeur á la liste*/
+        void ajouterSucc(const Sommet*s){
+            m_successeurs.push_back(s);
+        }
+        /* m»thode d'affichage*/
+        void afficher() const{
+            std::cout<<"     sommet "<<m_num<<" : ";
+            for (auto s : m_successeurs) std::cout<<s->getNum()<<" ";
+        }
+
+
         std::string getNom();
         void afficher();
         double getCoords1();
         double getCoords2();
+
+
 };
 
 
